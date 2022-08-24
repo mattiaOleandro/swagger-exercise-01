c6a19a95c30cf9c991d2aeeaab6932264c94c122
Mattia <oleandromattia@gmail.com>
2022-08-24 19:23:41 +0200

    Added swagger annotations in controllers

--- src/main/java/co/develhope/swaggerexercise01/controllers/DefaultController.java
+++ src/main/java/co/develhope/swaggerexercise01/controllers/DefaultController.java
@@ -1,7 +1,12 @@
 package co.develhope.swaggerexercise01.controllers;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class DefaultController {
 
     @GetMapping
+    @ApiOperation(value = "Default message", notes = "Print out the default message for the User")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses({
+            @ApiResponse(code = 202, message = "Created"),
+            @ApiResponse(code = 400, message = "Bad input data"),
+            @ApiResponse(code = 500, message = "Internal error from spring", response = String.class)
+    })
     public String getDefault(){
 
         return "Welcome on exercise Swagger 01";
--- src/main/java/co/develhope/swaggerexercise01/controllers/NameController.java
+++ src/main/java/co/develhope/swaggerexercise01/controllers/NameController.java
@@ -1,15 +1,23 @@
 package co.develhope.swaggerexercise01.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/name")
 public class NameController {
 
     @GetMapping(value = "/user")
+    @ApiOperation(value = "Name user", notes = "Print out the message for the User")
+    @ResponseStatus(HttpStatus.OK)
+    @ApiResponses({
+            @ApiResponse(code = 202, message = "Created"),
+            @ApiResponse(code = 400, message = "Bad input data"),
+            @ApiResponse(code = 500, message = "Internal error from spring", response = String.class)
+    })
     public String getName(@RequestParam String name){
 
         return "The name of user is " + name;
